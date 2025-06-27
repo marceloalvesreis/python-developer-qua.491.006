08bcd9a9b7dd9d693bf773334d55709fa6759262
marceloalvesreis <fariasmarcelinho0310@gmail.com>
2025-06-26 21:35:37 -0300

    dia 26/06, aprendemos o inicio de dicionario

---
+++ projetos_console/34_dicionario_parte_02/main.py
@@ -0,0 +1,13 @@
+# dicionário
+pessoa = {
+    'nome': "Fulano de Tal",
+    'idade': 18,
+    'email': "fulano@gmail.com",
+    "profissão": "DBA"
+}
+
+# exibe os valores 
+print(f"Nome: {pessoa.get('nome')}")
+print(f"Idade: {pessoa.get('idade')}")
+print(f"E-mail: {pessoa.get('email')}")
+print(f"Profissão: {pessoa.get('profissão')}")
---
+++ projetos_console/36_dicionario_parte_04/main.py
@@ -0,0 +1,5 @@
+# dicionário
+usuario = dict(nome="Alex Machado", idade=40, email="alex@gmail.com")
+
+for chave in usuario:
+    print(f"{chave.capitalize()}: {usuario.get(chave)}")
