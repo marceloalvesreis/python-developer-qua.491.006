1efd98d8ea431d60fdddbe83bbd49ec9425b8e46
marceloalvesreis <fariasmarcelinho0310@gmail.com>
2025-08-27 21:27:13 -0300

    aula do dia 27/08

--- banco_conexao/01_crud_uma_entidade/main.py
+++ banco_conexao/01_crud_uma_entidade/main.py
@@ -1,22 +1,90 @@
 # importar a biblioteca sqlalchemy
 from sqlalchemy import create_engine, Column, Integer, String, Date
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
+from datetime import datetime
+import os
 
-try:
+limpar = lambda: os.system("cls" if os.name == "nt" else "clear")
+
+def criar_tb_pessoa(engine, Base):
+    try:
+        class Pessoa(Base):
+            __tablename__ = "pessoa"
+
+            id_pessoa = Column(Integer, primary_key=True, autoincrement=True)
+            nome = Column(String, nullable=False)
+            email = Column(String, nullable=False, unique=True)
+            data_nascimento = Column(Date, nullable=False)
+
+        Base.metadata.create_all(engine)
+        
+    # NOTE: engine para o Mysql
+    # engine = create_engine("mysql+mysqlconnector://senha:root@localhost:3306/nome_do_banco")
+        return Pessoa
+    except Exception as e:
+        print(f"Não foi possível conectar ao banco. {e}")
+
+def cadastrar_pessoa(session, Pessoa):
+    nome = input("Digite o nome do usuário: ").strip().title()
+    email = input("Digite o email do usuário: ").strip().lower()
+    data_nascimento = input("Digite a data de nascimento (dd/mm/aaaa): ").strip()
+    data_nascimento = datetime.strptime(data_nascimento, "%d/%m/%Y").date()
+
+    nova_pessoa = Pessoa(nome=nome, email=email, data_nascimento=data_nascimento)
+
+    session.add(nova_pessoa)
+    session.commit()
+    print(f"Pessoa {nome} cadastrada com sucesso.")
+
+def listar_pessoas(session, Pessoa):
+    pessoas = session.query(Pessoa).all()
+
+    print("\nPessoas cadastradas:\n")
+    for pessoa in pessoas:
+        print(f"ID: {pessoa.id_pessoa}")
+        print(f"Nome: {pessoa.nome}")
+        print(f"Email: {pessoa.email}")
+        print(f"Data de Nascimento: {pessoa.data_nascimento.strftime("%d/%m/%Y")}")
+        print(f"{'-'*40}")
+
+def main():
     engine = create_engine("sqlite:///01_crud_uma_entidade/database/db_crud.db")
     Base = declarative_base()
 
-    class Pessoa(Base):
-        __tablename__ = "pessoa"
+    Pessoa = criar_tb_pessoa(engine, Base)
 
-        id_pessoa = Column(Integer, primary_key=True, autoincrement=True)
-        nome = Column(String, nullable=False)
-        email = Column(String, nullable=False, unique=True)
-        data_nascimento = Column(Date, nullable=False)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    Base.metadata.create_all(engine)
-    
-    # NOTE: engine para o Mysql
-    # engine = create_engine("mysql+mysqlconnector://senha:root@localhost:3306/nome_do_banco")
-except Exception as e:
-    print(f"Não foi possível conectar ao banco. {e}")
+    limpar()
+    while True:
+        print(f"{'-'*20} CRUD DA SERPENTE {'-'*20}")
+        print("1 - Cadastrar nova pessoa")
+        print("2 - Listar pessoas")
+        print("3 - Alterar dados de uma pessoa")
+        print("4 - Excluir uma pessoa")
+        print("5 - Sair do programa")
+        opcao = input("Informe a opção desejada: ").strip()
+        limpar()
+        match opcao:
+            case "1":
+                cadastrar_pessoa(session, Pessoa)
+                continue
+            case "2":
+                listar_pessoas(session, Pessoa)
+                continue
+            case "3":
+                pass
+            case "4":
+                pass
+            case "5":
+                print("Programa encerrado.")
+                break
+            case _:
+                print("Opção inválida.")
+                continue
+
+    session.close()
+
+if __name__ == "__main__":
+    main()
